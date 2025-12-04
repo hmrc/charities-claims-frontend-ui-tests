0b2cf19c71f66d4702983a1532373e8c0e603dc5
jordanriddle-dev <242013470+jordanriddle-dev@users.noreply.github.com>
2025-12-04 11:40:17 +0000

    Adding page object for 'What is the name of the charity regulator?' page

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/WhatIsTheNameOfTheCharityRegulatorPage.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+object WhatIsTheNameOfTheCharityRegulatorPage extends BasePage {
+
+  override def pageUrl: String = s"$hostname/name-of-charity-regulator"
+
+  override def pageTitle: String =
+    "What is the name of the charity regulator? - Charities - GOV.UK"
+
+  def pageCaption: String =
+    "Provide organisation details"
+
+  def pageHeader: String =
+    "What is the name of the charity regulator?"
+
+  def pageErrorMsg: String =
+    "Select the name of the charity regulator"
+
+  val EngWal: String = "#nameOfCharityReg"
+  val Ire: String  = "#nameOfCharityReg-2"
+  val None: String  = "#nameOfCharityReg-3"
+  val Scot: String  = "#nameOfCharityReg-4"
+
+  def validateNavigation(): Unit = {
+    WhatIsTheNameOfTheCharityRegulatorPage.verifyPageUrl(WhatIsTheNameOfTheCharityRegulatorPage.pageUrl)
+    WhatIsTheNameOfTheCharityRegulatorPage.verifyPageTitle(WhatIsTheNameOfTheCharityRegulatorPage.pageTitle)
+    WhatIsTheNameOfTheCharityRegulatorPage.verifyPageHeader(WhatIsTheNameOfTheCharityRegulatorPage.pageHeader)
+  }
+
+  /** Validate that the error message is correct */
+  def validateErrorMessage(): Unit =
+    WhatIsTheNameOfTheCharityRegulatorPage.validateGenericPageError(WhatIsTheNameOfTheCharityRegulatorPage.pageErrorMsg)
+}
