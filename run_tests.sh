c1bfea245bc7fe4380a2cc83726fd1a4e3dc637c
sanchitbhatnagar <239178408+sanchitbhatnagar@users.noreply.github.com>
2025-12-12 16:38:12 +0000

    run file sh update

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 BROWSER=$1
 ENVIRONMENT=$2
-sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Daccessibility.timeout=5000.millis -Denvironment="${ENVIRONMENT:=QA}" -Dbrowser.option.headless=false "testOnly uk.gov.hmrc.ui.specs.GiftAidJourneySpec" testReport
+sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Daccessibility.timeout=5000.millis -Denvironment="${ENVIRONMENT:=local}" -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.ui.specs.GiftAidJourneySpec" testReport
