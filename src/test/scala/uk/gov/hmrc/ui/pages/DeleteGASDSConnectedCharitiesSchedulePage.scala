3fa7dcffa1dc2b6185a5eef25112bf2e6e83cb34
jordanriddle-dev <242013470+jordanriddle-dev@users.noreply.github.com>
2026-01-14 09:47:03 +0000

    Warning screens baseline

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/DeleteGASDSConnectedCharitiesSchedulePage.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+import org.openqa.selenium.By
+
+object DeleteGASDSConnectedCharitiesSchedulePage extends BasePage {
+
+  override def pageUrl: String = s"$hostname/delete-gasds-connected-charities-schedule"
+
+  override def pageTitle: String =
+    "Do you want to delete this Gift Aid Small Donations Scheme connected charities schedule? - Charities - GOV.UK"
+
+  def pageHeader: String =
+    "Do you want to delete this Gift Aid Small Donations Scheme connected charities schedule?"
+
+  def pageErrorMsg: String =
+    "Select ‘Yes’ if you want to delete this Gift Aid Small Donations Scheme connected charities schedule"
+
+  val yes: String = "#value"
+  val no: String = "#value-no"
+  val errorMsgLocator = By.ById("value-error")
+
+  def validateNavigation(): Unit = {
+    DeleteGASDSConnectedCharitiesSchedulePage.verifyPageUrl(DeleteGASDSConnectedCharitiesSchedulePage.pageUrl)
+    DeleteGASDSConnectedCharitiesSchedulePage.verifyPageTitle(DeleteGASDSConnectedCharitiesSchedulePage.pageTitle)
+    DeleteGASDSConnectedCharitiesSchedulePage.verifyPageHeader(DeleteGASDSConnectedCharitiesSchedulePage.pageHeader)
+  }
+
+  /** Validate that the error message is correct */
+  def validateErrorMessage(): Unit =
+    DeleteGASDSConnectedCharitiesSchedulePage.validateGenericPageError(
+      DeleteGASDSConnectedCharitiesSchedulePage.pageErrorMsg,
+      DeleteGASDSConnectedCharitiesSchedulePage.errorMsgLocator
+    )
+}
