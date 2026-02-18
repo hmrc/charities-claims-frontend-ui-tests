b5c4510ec7ff1afb0bee1ff1d3292c855d2a18ba
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-02-18 13:02:30 +0000

    Beggining implementing O1.3

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/OtherIncomeTableNoErrorsPage.scala
@@ -0,0 +1,16 @@
+package uk.gov.hmrc.ui.pages
+
+object OtherIncomeTableNoErrorsPage extends BasePage {
+
+  override def pageUrl: String = s"$hostname/"
+
+  override def pageTitle: String = ""
+
+  def pageCaption: String = ""
+
+  def pageHeading: String = ""
+
+
+
+
+}
