e5037ad11f786b893bf7c5671f3f4baef6e73a89
Kyle <63915795+KezzaTezza99@users.noreply.github.com>
2026-03-04 12:37:25 +0000

    Feature/team2 clean up (#61)
    * CC1.3 POM and Spec
    
    * Removed CC Table from .3 as it causes errors with compound class names
    
    * CC1.4 POM and Spec, also renamed bad file name some reason would fail file upload
    
    * CC1.5 POM and Spec
    
    * Clean up for PR
    
    * Fixed a typo in OI spec

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/CheckYourConnectedCharitiesScheduleErrorPage.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+object CheckYourConnectedCharitiesScheduleErrorPage extends BasePage {
+
+  override def pageUrl: String = s"$hostname/problem-with-connected-charities-schedule"
+
+  override def pageTitle: String =
+    "There is a problem with the data in your Connected Charities schedule - Charities - GOV.UK"
+
+  def pageCaption: String =
+    "Add Connected Charities schedule"
+
+  def pageHeader: String =
+    "There is a problem with the data in your Connected Charities schedule"
+
+  def pageWarning: String =
+    "We cannot accept the file because it has errors. You must fix these errors and upload the updated file before you can continue."
+
+  def pageHeader2: String =
+    "What you can do next"
+
+  def pageHeader3: String =
+    "Summary of errors"
+
+  def errorHelp1: String =
+    "Refer to the Schedule spreadsheet to claim tax back on Connected Charities donations (opens in new tab)."
+
+  def errorHelp2: String =
+    "Delete this Connected Charities schedule."
+
+  def errorHelp3: String =
+    "Update your spreadsheet."
+
+  def errorHelp4: String =
+    "Upload the updated spreadsheet."
+
+  def validateNavigation(): Unit = {
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyPageUrl(CheckYourConnectedCharitiesScheduleErrorPage.pageUrl)
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyPageTitle(CheckYourConnectedCharitiesScheduleErrorPage.pageTitle)
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyPageCaption(
+      CheckYourConnectedCharitiesScheduleErrorPage.pageCaption
+    )
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyPageHeader(
+      CheckYourConnectedCharitiesScheduleErrorPage.pageHeader
+    )
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyPageWarning(
+      CheckYourConnectedCharitiesScheduleErrorPage.pageWarning
+    )
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyPageSubHeading1(
+      CheckYourConnectedCharitiesScheduleErrorPage.pageHeader2
+    )
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyPageSubHeading2(
+      CheckYourConnectedCharitiesScheduleErrorPage.pageHeader3
+    )
+  }
+
+  def validatePageContent(): Unit =
+    CheckYourConnectedCharitiesScheduleErrorPage.verifyScheduleErrorHelpList(
+      CheckYourConnectedCharitiesScheduleErrorPage.createSingleStringFromMany(
+        CheckYourConnectedCharitiesScheduleErrorPage.errorHelp1,
+        CheckYourConnectedCharitiesScheduleErrorPage.errorHelp2,
+        CheckYourConnectedCharitiesScheduleErrorPage.errorHelp3,
+        CheckYourConnectedCharitiesScheduleErrorPage.errorHelp4
+      )
+    )
+}
---
+++ src/test/scala/uk/gov/hmrc/ui/pages/CheckYourConnectedCharitiesSchedulePage.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+import org.openqa.selenium.By
+
+object CheckYourConnectedCharitiesSchedulePage extends BasePage {
+
+  override def pageUrl: String = s"$hostname/check-your-connected-charities-schedule"
+
+  override def pageTitle: String = "Check your Connected Charities schedule - Charities - GOV.UK"
+
+  def pageCaption: String = "Add Connected Charities schedule"
+
+  def pageHeading: String = "Check your Connected Charities schedule"
+
+  def pageErrorMsg: String = "Select ‘Yes’ if you need to update this Connected Charities schedule"
+
+  def textRadioButtonQuestion: String = "Do you need to update this Connected Charities schedule?"
+
+  def textRadioButtonYes: String = "Yes"
+
+  def textRadioButtonNo: String = "No, continue with Connected Charities schedule"
+
+  val yes: String     = "#value"
+  val no: String      = "#value-no"
+  val errorMsgLocator = By.ById("value-error")
+
+  def validateNavigation(): Unit = {
+    CheckYourConnectedCharitiesSchedulePage.verifyPageUrl(CheckYourConnectedCharitiesSchedulePage.pageUrl)
+    CheckYourConnectedCharitiesSchedulePage.verifyPageTitle(CheckYourConnectedCharitiesSchedulePage.pageTitle)
+    CheckYourConnectedCharitiesSchedulePage.verifyPageCaption(CheckYourConnectedCharitiesSchedulePage.pageCaption)
+    CheckYourConnectedCharitiesSchedulePage.verifyPageHeader(CheckYourConnectedCharitiesSchedulePage.pageHeading)
+  }
+
+  def validateFormFieldset(): Unit =
+    CheckYourConnectedCharitiesSchedulePage.verifyFormFieldsetContent(
+      CheckYourConnectedCharitiesSchedulePage.createSingleStringFromMany(
+        CheckYourConnectedCharitiesSchedulePage.textRadioButtonQuestion,
+        CheckYourConnectedCharitiesSchedulePage.textRadioButtonYes,
+        CheckYourConnectedCharitiesSchedulePage.textRadioButtonNo
+      )
+    )
+
+  /** Validate that the error message is correct */
+  def validateErrorMessage(): Unit =
+    CheckYourConnectedCharitiesSchedulePage.validateGenericPageError(
+      CheckYourConnectedCharitiesSchedulePage.pageErrorMsg,
+      CheckYourConnectedCharitiesSchedulePage.errorMsgLocator
+    )
+}
