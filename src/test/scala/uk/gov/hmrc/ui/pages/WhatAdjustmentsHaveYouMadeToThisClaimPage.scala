b87cb8599e02cd1b3385b34c7fdc34007c02572e
jordanriddle-dev <242013470+jordanriddle-dev@users.noreply.github.com>
2026-03-12 15:46:29 +0000

    Baseline for Declaration journey and D1.1

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/WhatAdjustmentsHaveYouMadeToThisClaimPage.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+import org.openqa.selenium.By
+
+object WhatAdjustmentsHaveYouMadeToThisClaimPage extends BasePage {
+
+  override def pageUrl: String = s"$hostname/adjustments-to-this-claim"
+
+  override def pageTitle: String =
+    "What adjustments have you made to this claim? - Charities - GOV.UK"
+
+  def pageHeader: String =
+    "What adjustments have you made to this claim?"
+
+  def pageParagraph: String =
+    "If you have included any adjustments in this claim, you must enter an explanation below. You can also provide any other information regarding your charity's repayment claim."
+
+  def pageHint: String =
+    "You can enter up to 350 characters"
+
+  def pageCharacterCount: String =
+    "You have 350 characters remaining"
+
+  def pageErrorMsgTooManyChars: String =
+    "Adjustments must be 350 characters or less"
+
+  val adjustmentDetailsFieldLocator = By.ById("value")
+  val errorMsgLocator = By.ById("value-error")
+
+  def inputMaxLength = 351
+
+  def validateNavigation(): Unit = {
+    WhatAdjustmentsHaveYouMadeToThisClaimPage.verifyPageUrl(WhatAdjustmentsHaveYouMadeToThisClaimPage.pageUrl)
+    WhatAdjustmentsHaveYouMadeToThisClaimPage.verifyPageTitle(WhatAdjustmentsHaveYouMadeToThisClaimPage.pageTitle)
+    WhatAdjustmentsHaveYouMadeToThisClaimPage.verifyPageHeader(WhatAdjustmentsHaveYouMadeToThisClaimPage.pageHeader)
+  }
+
+  /** Validate that the error message is correct */
+  def validateErrorMessage(): Unit =
+    WhatAdjustmentsHaveYouMadeToThisClaimPage.triggerTooManyCharInputError(
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.inputMaxLength,
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.pageErrorMsgTooManyChars,
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.adjustmentDetailsFieldLocator,
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.errorMsgLocator
+    )
+
+  def validateParagraph(): Unit =
+    WhatAdjustmentsHaveYouMadeToThisClaimPage.verifyParagraphText(
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.pageParagraph
+    )
+
+  def validatePageContent(): Unit =
+    WhatAdjustmentsHaveYouMadeToThisClaimPage.verifyEntirePageContent(
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.createSingleStringFromMany(
+        WhatAdjustmentsHaveYouMadeToThisClaimPage.pageHeader,
+        WhatAdjustmentsHaveYouMadeToThisClaimPage.pageParagraph,
+        WhatAdjustmentsHaveYouMadeToThisClaimPage.pageHint,
+        WhatAdjustmentsHaveYouMadeToThisClaimPage.pageCharacterCount
+      )
+    )
+}
---
+++ src/test/scala/uk/gov/hmrc/ui/specs/DeclarationJourneySpec.scala
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs
+
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.verbs.ShouldVerb
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen}
+import uk.gov.hmrc.selenium.webdriver.{Browser, ScreenshotOnFailure}
+import uk.gov.hmrc.ui.pages.*
+import uk.gov.hmrc.ui.util.Users.LoginTypes.HASDIRECT
+import uk.gov.hmrc.ui.util.Users.UserTypes.Organisation
+
+class DeclarationJourneySpec
+    extends AnyFeatureSpec
+    with BaseSpec
+    with GivenWhenThen
+    with ShouldVerb
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with Browser
+    with ScreenshotOnFailure {
+
+  Feature("Charities - Organisation - Declaration Page Validations") {
+    Scenario(
+      "User navigates to the 'What adjustments have you made to this claim?' page from the Gift Aid flow and validates the page elements"
+    ) {
+      Given("the user logs in through the Authority Wizard page")
+      AuthWizard.login(HASDIRECT, Organisation, "Organisation", "HMRC-CHAR-ORG", "CHARID", "PAGE-TEST-D1-1-GA")
+      Then("User navigates to 'Make a charity repayment claim' page")
+      ClaimsTaskListPage_Empty.validateNavigation()
+      And("User clicks the link to navigate to 'Repayment claim details' page")
+      ClaimsTaskListPage_Empty.clickProvideRepaymentClaimDetails()
+      Then("User validates the 'Repayment claim details' page")
+      RepaymentClaimDetailsPage.validateNavigation()
+      And("User clicks continue on 'Repayment claim details' page")
+      RepaymentClaimDetailsPage.clickContinue()
+      Then("User navigates to 'Which type of repayment claim do you want to make?' page")
+      RepaymentCheckboxPage.validateNavigation()
+      And("User selects 'Top up payments for donations under the GASDS' checkbox and clicks continue")
+      RepaymentCheckboxPage.checkbox(RepaymentCheckboxPage.GiftAid, true)
+      RepaymentCheckboxPage.clickContinue()
+      Then("User navigates to 'Do you have a claim reference number?' page")
+      DoYouHaveAClaimReferenceNumberPage.validateNavigation()
+      And("User selects 'No' and clicks continue on 'Do you have a claim reference number?' page")
+      DoYouHaveAClaimReferenceNumberPage.radioButton(DoYouHaveAClaimReferenceNumberPage.no)
+      DoYouHaveAClaimReferenceNumberPage.clickContinue()
+      Then("User navigates to 'Check your repayment claim' page")
+      CheckYourRepaymentClaimPage.validateNavigation()
+      CheckYourRepaymentClaimPage.clickContinue()
+      Then("User navigates to 'Make a charity repayment claim' page")
+      ClaimsTaskListPage_InProgress.validateNavigation()
+      And("User clicks the link to navigate to 'About the organisation' page")
+      ClaimsTaskListPage_InProgress.clickProvideOrganisationDetails()
+      AboutTheOrganisationPage.validateNavigation()
+      Then("User selects continue on 'About the organisation' page")
+      AboutTheOrganisationPage.clickContinue()
+      And("User navigates to 'What is the name of the charity regulator?' page")
+      WhatIsTheNameOfTheCharityRegulatorPage.validateNavigation()
+      And("User selects they are not registered with a regulator")
+      WhatIsTheNameOfTheCharityRegulatorPage.radioButton(WhatIsTheNameOfTheCharityRegulatorPage.None)
+      WhatIsTheNameOfTheCharityRegulatorPage.clickContinue()
+      And("User navigates to 'Why is the charity not registered with a regulator?' page")
+      WhyIsTheCharityNotRegisteredPage.validateNavigation()
+      And("User selects they are excepted")
+      WhyIsTheCharityNotRegisteredPage.radioButton(WhyIsTheCharityNotRegisteredPage.Excepted)
+      WhyIsTheCharityNotRegisteredPage.clickContinue()
+      And("User navigates to 'Your charity is excepted' page")
+      YourCharityIsExceptedPage.validateNavigation()
+      Then("User selects continue on 'Your charity is excepted' page")
+      YourCharityIsExceptedPage.clickContinue()
+      And("User navigates to 'Is a corporate trustee making this claim?' page")
+      IsACorporateTrusteeMakingThisClaimPage.validateNavigation()
+      And("User selects a corporate trustee is making this claim")
+      IsACorporateTrusteeMakingThisClaimPage.radioButton(IsACorporateTrusteeMakingThisClaimPage.yes)
+      IsACorporateTrusteeMakingThisClaimPage.clickContinue()
+      And("User navigates to 'Does the corporate trustee have a UK address?' page")
+      DoesTheCorporateTrusteeHaveAUKAddressPage.validateNavigation()
+      And("User selects a corporate trustee does not have a UK address")
+      DoesTheCorporateTrusteeHaveAUKAddressPage.radioButton(DoesTheCorporateTrusteeHaveAUKAddressPage.no)
+      DoesTheCorporateTrusteeHaveAUKAddressPage.clickContinue()
+      And("User navigates to 'What are the corporate trustee details?' page")
+      CorporateTrusteeDetailsPage.validateNavigation()
+      And("User enters their Non UK Trustee details and clicks continue")
+      CorporateTrusteeDetailsPage.enterNonUKTrusteeDetails("TEST TRUSTEE", "00-1-332-555-2368")
+      And("User navigates to 'Check your organisation details' page")
+      CheckYourOrganisationDetailsPage.validateNavigation()
+      Then("User navigates to 'Make a charity repayment claim' page")
+      CheckYourOrganisationDetailsPage.clickContinue()
+      ClaimsTaskListPage_InProgress.validateNavigation()
+      And("User clicks the link to navigate to 'Add Gift Aid schedule' page")
+      ClaimsTaskListPage_InProgress.clickAddGiftAidSchedule()
+      Then("User navigates to 'About Gift Aid schedule' page")
+      AboutGiftAidSchedulePage.validateNavigation()
+      AboutGiftAidSchedulePage.clickContinue()
+      Then("User navigates to 'Upload a Gift Aid schedule' page")
+      UploadAGiftAidSchedulePage.validateNavigation()
+      Then("User selects a file to upload in the 'Upload a Gift Aid schedule' page")
+      UploadAGiftAidSchedulePage.selectFile("GiftAidSpreadsheets/Gift-Aid-Schedule-SIMPLEPASS_MAX_ROWS")
+      UploadAGiftAidSchedulePage.clickContinue()
+      Then("User navigates to 'Your Gift Aid schedule upload' page")
+      YourGiftAidScheduleUploadPage.validateNavigation()
+      YourGiftAidScheduleUploadPage.waitForFileUpload()
+      YourGiftAidScheduleUploadPage.clickContinue()
+      Then("User navigates to 'Check your Gift Aid schedule' page")
+      CheckYourGiftAidSchedulePage.validateNavigation()
+      CheckYourGiftAidSchedulePage.radioButton(CheckYourGiftAidSchedulePage.no)
+      CheckYourGiftAidSchedulePage.clickContinue()
+      Then("User navigates to 'Successful Gift Aid Upload' page")
+      GiftAidUploadSuccessfulPage.validateNavigation()
+      Then("User navigates to 'Make a charity repayment claim' page")
+      GiftAidUploadSuccessfulPage.clickContinue()
+      ClaimsTaskListPage_InProgress.validateNavigation()
+      Then("User navigates to 'What adjustments have you made to this claim?' page")
+      ClaimsTaskListPage_InProgress.clickReadDeclaration()
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.validateNavigation()
+      And("User validates 'What adjustments have you made to this claim?' page elements")
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.validatePageContent()
+      And("User validates 'What adjustments have you made to this claim?' page error message")
+      WhatAdjustmentsHaveYouMadeToThisClaimPage.validateErrorMessage()
+    }
+  }
+}
