e655d5d88d30ef56e51ec12b1653ed886bd339ed
jordanriddle-dev <242013470+jordanriddle-dev@users.noreply.github.com>
2026-03-27 13:29:22 +0000

    Frameworking for D1.2 screen

--- src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
@@ -209,10 +209,10 @@ trait BasePage extends PageObject with Eventually with Matchers with LazyLogging
   }
 
   /** Specific actions */
-  def clickContinue(): Unit = click(By.xpath(Locators.btnContinue))
-  def clickBackLink(): Unit = click(By.linkText(Locators.lnkBack))
-//  def saveAndContinue(): Unit   = click(By.cssSelector(Locators.btnContinue))
-  def header(): Unit        = click(By.cssSelector(Locators.lnkHeader))
+  def clickContinue(): Unit         = click(By.xpath(Locators.btnContinue))
+  def clickBackLink(): Unit         = click(By.linkText(Locators.lnkBack))
+  def clickConfirmAndSubmit(): Unit = click(By.cssSelector(Locators.btnContinue))
+  def header(): Unit                = click(By.cssSelector(Locators.lnkHeader))
 //  def removeFile(): Unit        = click(By.cssSelector(Locators.lnkRemoveFile))
 
   def clickDeleteScheduleLink(): Unit = click(By.xpath(Locators.lnkDeleteSchedule))
