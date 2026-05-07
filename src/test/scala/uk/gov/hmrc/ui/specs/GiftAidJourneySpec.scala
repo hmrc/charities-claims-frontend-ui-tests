0bddb0454e38107bdbb2a9b2565504896f1b2bb0
sanchitbhatnagar <239178408+sanchitbhatnagar@users.noreply.github.com>
2026-05-07 07:14:14 +0100

    update repayment gift aid spec with NO Adj

--- src/test/scala/uk/gov/hmrc/ui/specs/GiftAidJourneySpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/GiftAidJourneySpec.scala
@@ -207,5 +207,57 @@ class GiftAidJourneySpec
         CheckYourRepaymentClaimPage.GASDSAdjustment        -> "Yes"
       )
     }
+
+    Scenario(
+      "User navigates to the 'Repayment claim details' page and validates the page elements - GASDS,GA,OI with reference number"
+    ) {
+      Given("the user logs in through the Authority Wizard page")
+      AuthWizard.login(HASDIRECT, Organisation, "Organisation", "HMRC-CHAR-ORG", "CHARID", "JOURNEYTEST - R4")
+      Then("User navigates to 'Make a charity repayment claim' page")
+      ClaimsTaskListPage_Empty.validateNavigation()
+      And("User clicks the link to navigate to 'Repayment claim details' page")
+      ClaimsTaskListPage_Empty.clickProvideRepaymentClaimDetails()
+      Then("User validates the 'Repayment claim details' page")
+      RepaymentClaimDetailsPage.validateNavigation()
+      And("User clicks continue on 'Repayment claim details' page")
+      RepaymentClaimDetailsPage.clickContinue()
+      Then("User navigates to 'Which type of repayment claim do you want to make?' page")
+      RepaymentCheckboxPage.validateNavigation()
+      And("User selects all of the checkboxes and clicks continue")
+      RepaymentCheckboxPage.checkbox(RepaymentCheckboxPage.GASDSclaim, true)
+      RepaymentCheckboxPage.clickContinue()
+      And("User navigates to 'Gift Aid Small Donations Scheme (GASDS) details' checkbox page")
+      GASDSCheckboxPage.validateNavigation()
+      Then("User validates entire page content on the 'Gift Aid Small Donations Scheme (GASDS) details' checkbox page")
+      GASDSCheckboxPage.validatePageContent()
+      And("User selects 'GASDS top-up', 'Community Building' and 'Connected Charities' checkbox and clicks continue")
+      GASDSCheckboxPage.checkbox(GASDSCheckboxPage.GASDSBuilding, true)
+      GASDSCheckboxPage.checkbox(GASDSCheckboxPage.GASDSConnected, true)
+      GASDSCheckboxPage.clickContinue()
+      Then("User navigates to 'Gift Aid Small Donations Scheme claim' page")
+      GiftAidSmallDonationsSchemeClaimPage.validateNavigation()
+      And("User selects 'No' and clicks continue on 'Gift Aid Small Donations Scheme claim' page")
+      GiftAidSmallDonationsSchemeClaimPage.radioButton(GiftAidSmallDonationsSchemeClaimPage.no)
+      GiftAidSmallDonationsSchemeClaimPage.clickContinue()
+      Then("User navigates to 'Do you have a claim reference number?' page")
+      DoYouHaveAClaimReferenceNumberPage.validateNavigation()
+      And("User selects 'Yes' and clicks continue on 'Do you have a claim reference number?' page")
+      DoYouHaveAClaimReferenceNumberPage.radioButton(DoYouHaveAClaimReferenceNumberPage.yes)
+      DoYouHaveAClaimReferenceNumberPage.clickContinue()
+      Then("User navigates to 'What is your claim reference number?' page")
+      WhatIsYourClaimReferenceNumberPage.validateNavigation()
+      And("User enters their claim reference number and clicks continue")
+      WhatIsYourClaimReferenceNumberPage.enterClaimReferenceNumber("TESTREF123")
+      Then("User navigates to 'Check your repayment claim' page")
+      CheckYourRepaymentClaimPage.validateNavigation()
+      Then("User Validates the Key and Value pairs on 'Check your repayment claim' page")
+      CheckYourRepaymentClaimPage.assertAllSummaryPairsExactlyAt(0)(
+        CheckYourRepaymentClaimPage.repaymentClaimType -> "Top-up payments for small cash donations under Gift Aid Small Donations Scheme (GASDS)",
+        CheckYourRepaymentClaimPage.claimReferenceProvided -> "Yes",
+        CheckYourRepaymentClaimPage.claimReferenceNumber -> "TESTREF123",
+        CheckYourRepaymentClaimPage.GASDSClaimType -> "Community building top-up payment Connected charity or CASC claim",
+        CheckYourRepaymentClaimPage.GASDSAdjustment -> "No"
+      )
+    }
   }
 }
